86ebade24477b10de17982e4b43cd804fe7d5ff8
Intizar <intizar@incento.kr>
2024-04-19 17:13:27 +0900

    user model, db commands, dbClient file

---
+++ prisma/migrations/20240419081115_initial_user_model/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "phoneNumber" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
