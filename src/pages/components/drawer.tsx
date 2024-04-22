53d97aa11ed33b6049e4c64f3798b81fd1f927af
Intizar <intizar@incento.kr>
2024-04-22 16:01:30 +0900

    capitalize react components

--- src/pages/components/drawer.tsx
+++ src/pages/components/drawer.tsx
@@ -1,10 +1,18 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
+import { IconButton, Paper, Tooltip } from '@mui/material';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 
 const drawerWidth = 300;
 
-export default function CustomDrawer() {
+interface CustomDrawerProps {
+  handleEditCategories: () => void;
+}
+
+export default function CustomDrawer({
+  handleEditCategories,
+}: CustomDrawerProps) {
   return (
     <Drawer
       variant="permanent"
@@ -12,9 +20,17 @@ export default function CustomDrawer() {
         width: drawerWidth,
         flexShrink: 0,
         [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+        position: 'relative',
       }}
     >
       <Box sx={{ overflow: 'auto' }}></Box>
+      <Paper className="h-12 w-full absolute bottom-0 bg-slate-100 flex justify-center">
+        <Tooltip title="Edit categories">
+          <IconButton onClick={handleEditCategories}>
+            <AddCircleIcon fontSize="large" color="primary" />
+          </IconButton>
+        </Tooltip>
+      </Paper>
     </Drawer>
   );
 }
--- src/pages/components/layout.tsx
+++ src/pages/components/layout.tsx
@@ -1,20 +1,21 @@
 import { Box } from '@mui/material';
-import { ReactNode } from 'react';
-import CustomAppBar from './appbar';
-import CustomDrawer from './drawer';
+import { ReactNode, useState } from 'react';
+import CustomAppBar from '@/pages/components/Appbar';
+import CustomDrawer from '@/pages/components/Drawer';
 
 export default function Layout({ children }: { children: ReactNode }) {
+  const [editCategoriesModal, setEditCategoriesModal] = useState(false);
   return (
     <Box sx={{ display: 'flex' }}>
-      {/* <CssBaseline /> */}
       <CustomAppBar />
-      <CustomDrawer />
+      <CustomDrawer handleEditCategories={() => setEditCategoriesModal(true)} />
       <Box
         component="main"
         sx={{ flexGrow: 1, p: 3, width: '100%', height: '100%', mt: '64px' }}
       >
         {children}
       </Box>
+      {editCategoriesModal && <div></div>}
     </Box>
   );
 }
