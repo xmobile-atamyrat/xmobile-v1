db7dbc15ce50931d559e2d0650e957b88d26ab40
ovadan-rgb <owadantashova@gmail.com>
2024-04-23 17:14:06 +0900

    give appbar const height and adjust body padding

--- src/pages/components/layout.tsx
+++ src/pages/components/layout.tsx
@@ -1,18 +1,19 @@
 import { Box } from '@mui/material';
 import { ReactNode, useState } from 'react';
-import CustomAppBar from '@/pages/components/Appbar';
 import CustomDrawer from '@/pages/components/Drawer';
 import EditCategoriesDialog from '@/pages/components/EditCategoriesDialog';
+import CustomAppBar from '@/pages/components/Appbar';
+import { appBarHeight } from '@/pages/lib/constants';
 
 export default function Layout({ children }: { children: ReactNode }) {
   const [editCategoriesModal, setEditCategoriesModal] = useState(false);
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: 'flex', pt: `${appBarHeight}px` }}>
       <CustomAppBar />
       <CustomDrawer handleEditCategories={() => setEditCategoriesModal(true)} />
       <Box
         component="main"
-        sx={{ flexGrow: 1, p: 3, width: '100%', height: '100%', mt: '64px' }}
+        sx={{ flexGrow: 1, p: 3, width: '100%', height: '100%' }}
       >
         {children}
       </Box>
