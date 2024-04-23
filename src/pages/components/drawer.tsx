659ab661d304f382d194eb85efb4c7d9d403010d
Intizar <intizar@incento.kr>
2024-04-23 16:58:41 +0900

    implement category context

--- src/pages/components/drawer.tsx
+++ src/pages/components/drawer.tsx
@@ -3,6 +3,8 @@ import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import { IconButton, Paper, Tooltip } from '@mui/material';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
+import { useCategoryContext } from '@/pages/lib/CategoryContext';
+import { useEffect } from 'react';
 
 const drawerWidth = 300;
 
@@ -13,6 +15,10 @@ interface CustomDrawerProps {
 export default function CustomDrawer({
   handleEditCategories,
 }: CustomDrawerProps) {
+  const { categories } = useCategoryContext();
+  useEffect(() => {
+    console.log(categories);
+  }, [categories]);
   return (
     <Drawer
       variant="permanent"
@@ -23,7 +29,9 @@ export default function CustomDrawer({
         position: 'relative',
       }}
     >
-      <Box sx={{ overflow: 'auto' }}></Box>
+      {categories?.length > 0 && (
+        <Box sx={{ overflow: 'auto' }}>{categories[0].name}</Box>
+      )}
       <Paper className="h-12 w-full absolute bottom-0 bg-slate-100 flex justify-center">
         <Tooltip title="Edit categories">
           <IconButton onClick={handleEditCategories}>
