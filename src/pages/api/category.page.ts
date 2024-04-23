5383698a53eef927ae26a893852017ec57a8eb0b
Intizar <intizar@incento.kr>
2024-04-23 11:06:56 +0900

    initial category post and get apis

--- src/pages/api/category.page.ts
+++ src/pages/api/category.page.ts
@@ -1,5 +1,7 @@
 import dbClient from '@/lib/dbClient';
+import { ResponseApi } from '@/pages/lib/types';
 import { Category, Product } from '@prisma/client';
+import { NextApiRequest, NextApiResponse } from 'next';
 
 export async function getCategory(
   categoryId: string,
@@ -18,8 +20,41 @@ export async function getCategory(
   return category;
 }
 
-export default async function handler() {
-  //   req: NextApiRequest,
-  //   res: NextApiResponse<ResponseApi>,
-  //
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseApi>,
+) {
+  const { body, method, query } = req;
+  if (method === 'GET') {
+    try {
+      if (query.categoryId == null)
+        return res
+          .status(400)
+          .json({ success: false, message: 'categoryId is not provided' });
+      const category = await getCategory(query.categoryId as string);
+      return res.status(200).json({ success: true, data: category });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ success: false, message: "Couldn't get categories" });
+    }
+  } else if (method === 'POST') {
+    try {
+      const { name, predecessorId } = body as Category;
+      const category = await dbClient.category.create({
+        data: {
+          name,
+          predecessorId,
+        },
+      });
+      return res.status(200).json({ success: true, data: category });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ success: false, message: "Couldn't create a new category" });
+    }
+  }
+  return res
+    .status(405)
+    .json({ success: false, message: 'Method not allowed' });
 }
