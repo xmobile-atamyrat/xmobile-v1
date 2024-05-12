fffb29e71f9d9cf9d8d992c351eefa715bf61102
Intizar <intizar.tashov22@gmail.com>
2024-05-12 22:12:12 +0900

    add seed.sh to apply dummy db data

---
+++ seed.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker run -v xmobile-v1_db:/target --name temp_container -d alpine sleep infinity
+docker cp dummy_db_data/data.tar.gz temp_container:/target
+docker exec temp_container tar -xzf /target/data.tar.gz -C /target
+docker exec temp_container rm /target/data.tar.gz
+docker stop temp_container && docker rm temp_container
