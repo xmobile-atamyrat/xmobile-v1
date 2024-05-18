2d5616c8f1fa75bed12f6099a8aa0b08a16178ba
Intizar <intizar.tashov22@gmail.com>
2024-05-19 01:56:50 +0900

    add created and migrated dates to all models

--- src/pages/api/user.page.ts
+++ src/pages/api/user.page.ts
@@ -1,49 +1,49 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { User } from '@prisma/client';
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { ResponseApi } from '@/pages/lib/types';
-import dbClient from '@/lib/dbClient';
+// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import { User } from '@prisma/client';
+// import type { NextApiRequest, NextApiResponse } from 'next';
+// import { ResponseApi } from '@/pages/lib/types';
+// import dbClient from '@/lib/dbClient';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<ResponseApi<User>>,
-) {
-  switch (req.method) {
-    case 'POST':
-      try {
-        const { email, name, password, phoneNumber }: User = req.body;
-        const user = await dbClient.user.create({
-          data: {
-            email,
-            name,
-            password,
-            phoneNumber,
-          },
-        });
-        return res.status(200).json({
-          success: true,
-          data: user,
-        });
-      } catch (error) {
-        return res.status(500).json({
-          success: false,
-          message: (error as Error).message,
-        });
-      }
-    case 'GET':
-      return res.status(200).json({
-        success: true,
-        data: {
-          id: 'asdf',
-          email: 'intizar.tashov22@gmail.com',
-          name: 'Intizar',
-          password: 'asdf',
-          phoneNumber: 'asdf',
-        },
-      });
-    default:
-      return res
-        .status(405)
-        .json({ success: false, message: 'Method not allowed' });
-  }
-}
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<ResponseApi<User>>,
+// ) {
+//   switch (req.method) {
+//     case 'POST':
+//       try {
+//         const { email, name, password, phoneNumber }: User = req.body;
+//         const user = await dbClient.user.create({
+//           data: {
+//             email,
+//             name,
+//             password,
+//             phoneNumber,
+//           },
+//         });
+//         return res.status(200).json({
+//           success: true,
+//           data: user,
+//         });
+//       } catch (error) {
+//         return res.status(500).json({
+//           success: false,
+//           message: (error as Error).message,
+//         });
+//       }
+//     case 'GET':
+//       return res.status(200).json({
+//         success: true,
+//         data: {
+//           id: 'asdf',
+//           email: 'intizar.tashov22@gmail.com',
+//           name: 'Intizar',
+//           password: 'asdf',
+//           phoneNumber: 'asdf',
+//         },
+//       });
+//     default:
+//       return res
+//         .status(405)
+//         .json({ success: false, message: 'Method not allowed' });
+//   }
+// }
