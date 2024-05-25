f8e36362b23be44ed417f3282825788f03ad0c78
Intizar <intizar.tashov22@gmail.com>
2024-05-25 19:17:23 +0900

    connect login logic

--- src/pages/signin/index.page.tsx
+++ src/pages/signin/index.page.tsx
@@ -8,8 +8,16 @@ import {
 } from '@mui/material';
 import CancelIcon from '@mui/icons-material/Cancel';
 import Link from 'next/link';
+import { ResponseApi } from '@/pages/lib/types';
+import { User } from '@prisma/client';
+import { useUserContext } from '@/pages/lib/UserContext';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 export default function Signin() {
+  const { setUser } = useUserContext();
+  const [errorMessage, setErrorMessage] = useState<string>();
+  const router = useRouter();
   return (
     <Box className="h-[100vh] flex justify-center items-center">
       <Paper
@@ -28,7 +36,29 @@ export default function Signin() {
         component="form"
         onSubmit={async (event) => {
           event.preventDefault();
-          console.log('Signin');
+
+          if (errorMessage) setErrorMessage(undefined);
+
+          const formData = new FormData(event.currentTarget);
+          const { email, password } = Object.fromEntries(formData.entries());
+
+          try {
+            const { success, data, message }: ResponseApi<User> = await (
+              await fetch('/api/user/signin', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ email, password }),
+              })
+            ).json();
+            if (message != null) {
+              setErrorMessage(message);
+            } else if (success && data != null) {
+              setUser(data);
+              router.push('/');
+            }
+          } catch (error) {
+            console.error(error);
+          }
         }}
       >
         <Box className="flex flex-col gap-1">
@@ -40,21 +70,46 @@ export default function Signin() {
           </Box>
           <Divider />
         </Box>
-        <TextField fullWidth required label="Your email address" type="email" />
-        <TextField fullWidth required label="Your password" type="password" />
-        <Button
+        <TextField
           fullWidth
-          variant="contained"
-          sx={{ textTransform: 'none' }}
-          size="large"
-          type="submit"
-        >
-          Sign in
-        </Button>
-        <Divider />
-        <Box className="flex flex-row justify-between">
-          <Button sx={{ textTransform: 'none' }}>Forgot password?</Button>
-          <Button sx={{ textTransform: 'none' }}>Sign up</Button>
+          required
+          label="Your email address"
+          type="email"
+          name="email"
+        />
+        <TextField
+          fullWidth
+          required
+          label="Your password"
+          type="password"
+          name="password"
+        />
+        <Box className="flex flex-col gap-2">
+          <Box className="flex flex-col gap-2 relative min-h-[70px]">
+            <Button
+              fullWidth
+              variant="contained"
+              sx={{ textTransform: 'none' }}
+              size="large"
+              type="submit"
+            >
+              Sign in
+            </Button>
+            {errorMessage != null && (
+              <Typography
+                color="error"
+                fontSize={14}
+                className="absolute bottom-0"
+              >
+                {errorMessage}
+              </Typography>
+            )}
+          </Box>
+          <Divider />
+          <Box className="flex flex-row justify-between">
+            <Button sx={{ textTransform: 'none' }}>Forgot password?</Button>
+            <Button sx={{ textTransform: 'none' }}>Sign up</Button>
+          </Box>
         </Box>
       </Paper>
     </Box>
