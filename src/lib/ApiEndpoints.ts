1354cda5729387a7c8a7fc349ed26a6a551d8d20
Intizar <intizar.tashov22@gmail.com>
2024-07-13 17:32:44 +0900

    build related changes

--- src/lib/ApiEndpoints.ts
+++ src/lib/ApiEndpoints.ts
@@ -1,5 +1,3 @@
-const BASE_URL =
-  process.env.NEXT_PUBLIC_NGROK ||
-  `http://${process.env.NEXT_PUBLIC_HOST || 'localhost'}:${process.env.NEXT_PUBLIC_PORT || '3000'}`;
+const BASE_URL = `http://${process.env.NEXT_PUBLIC_HOST || 'localhost'}:${process.env.NEXT_PUBLIC_PORT || '3000'}`;
 
 export default BASE_URL;
