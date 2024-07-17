6eae763e59ec63daf4b51d6e8820817a20fe0fda
Intizar <intizar.tashov22@gmail.com>
2024-07-17 19:50:28 +0900

    add snackbar messages on the client side

--- scripts/add-translation-row.mjs
+++ scripts/add-translation-row.mjs
@@ -1,12 +1,12 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable no-console */
-import { readdir, readFile, writeFile } from 'node:fs';
-import { join, basename, dirname } from 'node:path';
-import { fileURLToPath } from 'url';
 import { translate } from '@vitalets/google-translate-api'; // or any other translation library
+import { readdir, readFile, writeFile } from 'node:fs';
+import { basename, dirname, join } from 'node:path';
+import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const i18nDir = join(__dirname, 'src', 'i18n');
+const i18nDir = join(__dirname, '../src', 'i18n');
 
 readdir(i18nDir, (err, files) => {
   if (err) {
