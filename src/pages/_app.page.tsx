6b15af9f663f740566f6966848d57feaa1bc28d8
Intizar <intizar.tashov22@gmail.com>
2024-08-09 20:05:25 +0900

    update logo and main color

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,7 +1,9 @@
 import CategoryContextProvider from '@/pages/lib/CategoryContext';
 import ProductContextProvider from '@/pages/lib/ProductContext';
 import UserContextProvider from '@/pages/lib/UserContext';
+import { theme } from '@/pages/lib/utils';
 import '@/styles/globals.css';
+import { ThemeProvider } from '@mui/material';
 import { NextIntlClientProvider } from 'next-intl';
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
@@ -9,18 +11,20 @@ import { useRouter } from 'next/router';
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   return (
-    <UserContextProvider>
-      <CategoryContextProvider>
-        <ProductContextProvider>
-          <NextIntlClientProvider
-            locale={router.locale}
-            timeZone="Asia/Ashgabat"
-            messages={pageProps.messages}
-          >
-            <Component {...pageProps} />
-          </NextIntlClientProvider>
-        </ProductContextProvider>
-      </CategoryContextProvider>
-    </UserContextProvider>
+    <ThemeProvider theme={theme}>
+      <UserContextProvider>
+        <CategoryContextProvider>
+          <ProductContextProvider>
+            <NextIntlClientProvider
+              locale={router.locale}
+              timeZone="Asia/Ashgabat"
+              messages={pageProps.messages}
+            >
+              <Component {...pageProps} />
+            </NextIntlClientProvider>
+          </ProductContextProvider>
+        </CategoryContextProvider>
+      </UserContextProvider>
+    </ThemeProvider>
   );
 }
