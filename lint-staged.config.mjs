5c4b8332058060dd280e577067d2c69477a3557c
Intizar <intizar.tashov22@gmail.com>
2024-08-19 23:48:41 +0900

    cleanup lint staged

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -27,16 +27,3 @@ export default (allStagedFiles) => {
     'npx tsc --noEmit --pretty -p tsconfig.json',
   ];
 };
-
-// module.exports = {
-//   '**/*.(ts|tsx)': () => 'npx tsc --noEmit --pretty -p tsconfig.json',
-
-//   // Lint & Prettify TS and JS files
-//   '**/*.(ts|tsx|js)': [
-//     'eslint --fix ./ --quiet',
-//     'npx prettier --write --ignore-unknown .',
-//   ],
-
-//   // Prettify only Markdown and JSON files
-//   '**/*.(md|json)': 'npx prettier --write --ignore-unknown .',
-// };
--- scripts/update-production.js
+++
@@ -1,5 +0,0 @@
-const args = process.argv.slice(2);
-
-console.info('Running update-production script');
-
-console.info(args);
