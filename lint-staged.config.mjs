3ade445bb216c11158f98286529465202575708f
Intizar <intizar.tashov22@gmail.com>
2024-08-20 00:07:39 +0900

    increase image quality

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,4 @@
 /* eslint-disable no-restricted-syntax */
-import { execSync } from 'child_process';
 
 const prefix = '/Users/intizar/Intizar/xmobile-v1/';
 
@@ -7,17 +6,18 @@ export default (allStagedFiles) => {
   for (const file of allStagedFiles) {
     if (file.startsWith(prefix)) {
       const relativePath = file.slice(prefix.length);
-      const scpCommand = `scp -i ~/.ssh/xmobile -P 2222 ${relativePath} ubuntu@216.250.13.115:/home/ubuntu/xmobile-v1/${relativePath}`;
-      execSync(scpCommand, (error, _, stderr) => {
-        if (error) {
-          console.error(`Error executing SCP: ${error.message}`);
-          return;
-        }
-        if (stderr) {
-          console.error(`SCP stderr: ${stderr}`);
-        }
-      });
-      console.info(`Copied ${relativePath} to production\n`);
+      const scpCommand = `scp -i ~/.ssh/xmobile -P 2222 ${relativePath} ubuntu@216.250.13.115:/home/ubuntu/xmobile-v1/${relativePath}\n`;
+      console.info(scpCommand);
+      //   execSync(scpCommand, (error, _, stderr) => {
+      //     if (error) {
+      //       console.error(`Error executing SCP: ${error.message}`);
+      //       return;
+      //     }
+      //     if (stderr) {
+      //       console.error(`SCP stderr: ${stderr}`);
+      //     }
+      //   });
+      //   console.info(`Copied ${relativePath} to production\n`);
     }
   }
 
