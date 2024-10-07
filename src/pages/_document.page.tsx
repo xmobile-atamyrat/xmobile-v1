f638ffb5721d2660a854316a20a41448973065aa
Intizar <intizar.tashov22@gmail.com>
2024-10-08 03:25:59 +0900

    potential fix for client side error

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className="notranslate" translate="no">
       <Head>
         <link rel="icon" href="/xm-logo.ico" />
       </Head>
