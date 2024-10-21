94d0a32bf87db3b38c6e28711b9817a3dabe6e38
Intizar <intizar.tashov22@gmail.com>
2024-10-21 19:01:35 +0900

    session storage caching category images:

--- src/pages/lib/utils.ts
+++ src/pages/lib/utils.ts
@@ -319,3 +319,12 @@ export const setCookie = (
   const serializedCookie = cookie.serialize(name, value, options);
   document.cookie = serializedCookie;
 };
+
+export const blobToBase64 = (blob: Blob): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onloadend = () => resolve(reader.result as string);
+    reader.onerror = reject;
+    reader.readAsDataURL(blob);
+  });
+};
