0d9e2e8a71211febd713dd551b59e884bb28794a
Intizar <intizar.tashov22@gmail.com>
2024-11-11 22:56:43 +0900

    update lint staged

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,35 +1,7 @@
-/* eslint-disable no-restricted-syntax */
-
-// const prefix = '/Users/intizar/Intizar/xmobile-v1/';
-
-export default () => {
-  // for (const file of allStagedFiles) {
-  //   if (
-  //     file.endsWith('.ts') ||
-  //     file.endsWith('.tsx') ||
-  //     file.endsWith('.sql') ||
-  //     file.includes('tsconfig.json') ||
-  //     file.includes('schema.prisma') ||
-  //     file.includes('next.config')
-  //   ) {
-  //     const relativePath = file.slice(prefix.length);
-  //     const scpCommand = `scp -i ~/.ssh/xmobile -P 2222 ${relativePath} ubuntu@216.250.13.115:/home/ubuntu/xmobile-v1/${relativePath}`;
-  //     execSync(scpCommand, (error, _, stderr) => {
-  //       if (error) {
-  //         console.error(`Error executing SCP: ${error.message}`);
-  //         return;
-  //       }
-  //       if (stderr) {
-  //         console.error(`SCP stderr: ${stderr}`);
-  //       }
-  //     });
-  //     console.info(`Copied ${relativePath} to production\n`);
-  //   }
-  // }
-
-  return [
+export default {
+  '*.{ts,tsx,js}': (stagedFiles) => [
     'eslint --fix .',
-    'npx prettier --write --ignore-unknown .',
-    'npx tsc --noEmit --pretty -p tsconfig.json',
-  ];
+    `prettier --write --ignore-unknown ${stagedFiles.join(' ')}`,
+    'tsc --noEmit --pretty -p tsconfig.json',
+  ],
 };
