6bc5ccb7dc014a0358e6cc202f09da69fd5a4165
Intizar <intizar.tashov22@gmail.com>
2024-11-12 20:17:05 +0900

    ignore backup-data.sh

--- scripts/backup-production-data.sh
+++ scripts/backup-production-data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-scp -i ~/.ssh/xmobile -P 2222 ubuntu@216.250.13.115:/home/ubuntu/db_backup/db_backup.sql /Users/intizar/Intizar/xmobile-v1/backup/db_backup.sql
-scp -i ~/.ssh/xmobile -P 2222 ubuntu@216.250.13.115:/home/ubuntu/tar-file/images.tar.gz /Users/intizar/Intizar/xmobile-v1/backup/images.tar.gz
+scp xmobile:/home/ubuntu/db_backup/db_backup.sql ./backup/db_backup.sql
+scp xmobile:/home/ubuntu/tar-file/images.tar.gz ./backup/images.tar.gz
