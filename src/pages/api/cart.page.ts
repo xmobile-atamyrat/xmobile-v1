4d0de962b64e2c82eb35f15f2036593f60095cb5
babahan <babakulyyevbabahan@gmail.com>
2025-01-21 20:37:06 +0900

    Add POST/PUT api's to cart and debounce to packages

--- src/pages/api/cart.page.ts
+++ src/pages/api/cart.page.ts
@@ -12,6 +12,7 @@ const FormSchema = z.object({
 });
 
 const CreateCartItem = FormSchema.omit({ cartItemId: true });
+const EditCartItem = FormSchema.omit({ userId: true, productId: true });
 
 export default async function handler(
   req: NextApiRequest,
@@ -19,7 +20,7 @@ export default async function handler(
 ) {
   const data = req.body;
 
-  if (req.method === 'PUT') {
+  if (req.method === 'POST') {
     try {
       // validate the data input
       const { userId, productId, quantity } = CreateCartItem.parse({
@@ -28,21 +29,24 @@ export default async function handler(
         quantity: data.quantity,
       });
 
-      await dbClient.cartItems.upsert({
-        where: {
-          userId_productId: { userId, productId },
-        },
-        update: {
-          quantity: { increment: quantity },
-        },
-        create: {
-          userId,
-          productId,
-          quantity,
-        },
+      // delete if product and cart has a relation
+      const cartItemExist = await dbClient.cartItem.findFirst({
+        where: { userId, productId },
       });
 
-      res.status(200).json({ success: true, data: { quantity } });
+      if (!cartItemExist) {
+        await dbClient.cartItem.create({
+          data: {
+            userId,
+            productId,
+            quantity,
+          },
+        });
+
+        res.status(200).json({ success: true, data: { quantity } });
+      } else {
+        res.status(400).json({ success: false, message: 'cartItemExistError' });
+      }
     } catch (error) {
       res.status(400).json({ success: false, message: error.message });
     }
@@ -54,15 +58,26 @@ export default async function handler(
         quantity: true,
       }).parse(req.query);
 
-      // get cartItem, Products by userId
-      const cartItems = await dbClient.cartItems.findMany({
+      // get cartItem
+      const cartItems = await dbClient.cartItem.findMany({
         where: {
           userId,
         },
-        include: { product: true },
+        // include: {product}
       });
 
-      res.status(200).json({ success: true, data: cartItems });
+      // get products itself from cartItems
+      // 1. remove code below, when there's relation between product and cartItem; 2. uncomment "include: {product}" above.
+      const products = await Promise.all(
+        cartItems.map(async (cartItem) => {
+          const product = await dbClient.product.findUnique({
+            where: { id: cartItem.productId },
+          });
+          return { ...cartItem, product };
+        }),
+      );
+
+      res.status(200).json({ success: true, data: products });
     } catch (error) {
       if (error instanceof z.ZodError) {
         res.status(400).json({
@@ -76,7 +91,7 @@ export default async function handler(
     }
   } else if (req.method === 'DELETE') {
     try {
-      await dbClient.cartItems.delete({
+      await dbClient.cartItem.delete({
         where: { id: data.id },
       });
       res.status(200).json({ success: true, data: [] });
@@ -84,6 +99,40 @@ export default async function handler(
       res
         .status(400)
         .json({ success: false, message: "Couldn't delete product from cart" });
+    }
+  } else if (req.method === 'PUT') {
+    try {
+      const { cartItemId, quantity } = EditCartItem.parse({
+        cartItemId: data.id,
+        quantity: data.quantity,
+      });
+
+      const cartItem = await dbClient.cartItem.update({
+        where: { id: cartItemId },
+        data: {
+          quantity,
+        },
+      });
+
+      if (!cartItem.quantity) {
+        res.status(400).json({
+          success: false,
+          message: `${filepath}: cartItem was not found`,
+        });
+      } else {
+        res
+          .status(200)
+          .json({ success: true, data: { quantity: cartItem.quantity } });
+      }
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        res.status(400).json({
+          success: false,
+          message: `${filepath}: quantity/cartId type is incorrect`,
+        });
+      }
+
+      res.status(500).json({ success: false, message: error.message });
     }
   }
 }
