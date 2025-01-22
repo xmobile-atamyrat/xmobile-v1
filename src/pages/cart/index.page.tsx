4b9c326c158beae87f19a685938c1db98529a302
babahan <babakulyyevbabahan@gmail.com>
2025-01-22 17:54:24 +0900

    add breadcrumbs and continue shopping button to cart page and cleanup

--- src/pages/cart/index.page.tsx
+++ src/pages/cart/index.page.tsx
@@ -3,10 +3,21 @@ import Layout from '@/pages/components/Layout';
 import ProductCard from '@/pages/components/ProductCard';
 import { appBarHeight, mobileAppBarHeight } from '@/pages/lib/constants';
 import { useUserContext } from '@/pages/lib/UserContext';
-import { Box, useMediaQuery, useTheme } from '@mui/material';
+import {
+  Box,
+  Typography,
+  useMediaQuery,
+  useTheme,
+  Link,
+  IconButton,
+  Breadcrumbs,
+  CircularProgress,
+} from '@mui/material';
 import { GetStaticProps } from 'next';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
+import HomeIcon from '@mui/icons-material/Home';
+import { useTranslations } from 'next-intl';
 
 // getStaticProps because translations are static
 export const getStaticProps = (async (context) => {
@@ -21,17 +32,17 @@ export default function CartPage() {
   const theme = useTheme();
   const isMdUp = useMediaQuery(theme.breakpoints.up('md'));
   const { user } = useUserContext();
-  const [cartItems, setCartItems] = useState<any[]>([]);
+  const [cartItems, setCartItems] = useState([]);
   const router = useRouter();
+  const t = useTranslations();
 
-  // ondelete update cartitems
   const onDelete = (cartItemId: string) => {
     setCartItems(cartItems.filter((cartItem) => cartItem.id !== cartItemId));
   };
 
   useEffect(() => {
     const fetchUserCartItems = async (userId: string | undefined) => {
-      // fix this to redirect to login page
+      // TODO: fix this to redirect to login page
       if (!userId) {
         return;
       }
@@ -60,22 +71,44 @@ export default function CartPage() {
           mt: isMdUp ? `${appBarHeight}px` : `${mobileAppBarHeight}px`,
         }}
       >
-        {/* <SimpleBreadcrumbs /> */}
+        <Breadcrumbs separator="›" maxItems={2} className="ml-3">
+          <Link
+            href="/"
+            className="flex fle-row justify-center items-center gap-1 py-2"
+          >
+            <HomeIcon sx={{ fontSize: 18 }} />
+            <Typography fontSize={15}>{t('home')}</Typography>
+          </Link>
+          <Link href="/cart">
+            <Typography>{t('cart')}</Typography>
+          </Link>
+        </Breadcrumbs>
+
         <Box className="flex flex-wrap gap-4 w-full p-3">
-          {cartItems !== null &&
-            cartItems?.length > 0 &&
-            cartItems?.map((cartItem, idx) => (
-              <ProductCard
-                product={cartItem?.product}
-                key={idx}
-                cartProps={{
-                  cartAction: 'delete',
-                  quantity: cartItem?.quantity,
-                  cartItemId: cartItem?.id,
-                  onDelete,
-                }}
-              ></ProductCard>
-            ))}
+          {cartItems != null && cartItems.length > 0 ? (
+            <Suspense fallback={<CircularProgress />}>
+              {cartItems.map((cartItem, idx) => (
+                <ProductCard
+                  product={cartItem?.product}
+                  key={idx}
+                  cartProps={{
+                    cartAction: 'delete',
+                    quantity: cartItem?.quantity,
+                    cartItemId: cartItem?.id,
+                    onDelete,
+                  }}
+                />
+              ))}
+            </Suspense>
+          ) : (
+            <Box className="text-center w-full">
+              <Link href="/">
+                <IconButton className="mt-2 px-4 py-2 bg-blue-500 text-white text-sm rounded hover:bg-blue-600">
+                  {t('continueShopping')}
+                </IconButton>
+              </Link>
+            </Box>
+          )}
         </Box>
       </Box>
     </Layout>
