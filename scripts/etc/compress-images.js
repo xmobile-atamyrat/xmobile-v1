d79532f5a5bd41f09d591f2cf5bbe491271d2eef
babahan <babakulyyevbabahan@gmail.com>
2025-02-09 22:11:32 +0900

    [script] compress all images recursively near 100KB without wasting quality

--- scripts/etc/compress-images.js
+++ scripts/etc/compress-images.js
@@ -2,7 +2,7 @@ const sharp = require('sharp');
 const fs = require('fs');
 const path = require('path');
 
-const IMAGE_COMPRESSION_QUALITY = 5;
+const IMAGE_COMPRESSION_QUALITY = 40;
 const productionImgsPath = '/home/ubuntu/images';
 const localImgsPath = `${__dirname}/../../src/db/images`;
 
@@ -18,21 +18,28 @@ if (!fs.existsSync(compressedImgsPath)) fs.mkdirSync(compressedImgsPath);
 if (fs.existsSync(productImgsPath)) {
   const imgNames = fs.readdirSync(productImgsPath);
 
-  imgNames.forEach((imgName) => {
+  imgNames.forEach(async (imgName) => {
     const imgPath = path.join(productImgsPath, imgName);
 
     if (fs.readFileSync(imgPath)) {
       const img = fs.readFileSync(imgPath);
+      const outputPath = compressedImgsPath + imgName;
+      let compressedImg = img;
+      let quality = 85; // start compressing from 85% upto 40% (IMAGE_COMPRESSION_QUALITY)
 
-      if (img.length > 100 * 1024) {
-        // don't compress images under 100KB
-        const outputPath = compressedImgsPath + imgName;
-        sharp(img)
-          .png({ quality: IMAGE_COMPRESSION_QUALITY })
-          .toFile(outputPath); // reduce quality to 5% of original image
-
-        // console.log('Compressing..: ' + outputPath);
+      while (
+        compressedImg.length > 100 * 1024 &&
+        quality > IMAGE_COMPRESSION_QUALITY
+      ) {
+        // max compression around 100KB
+        try {
+          compressedImg = await sharp(img).jpeg({ quality }).toBuffer();
+          quality -= 10;
+        } catch (error) {
+          console.error('failed to compress: ', outputPath);
+        }
       }
+      fs.writeFileSync(outputPath, compressedImg);
     } else console.error('Incorrect path!: ', imgPath);
   });
 }
