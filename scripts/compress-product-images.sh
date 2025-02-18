08d7b7b5970ad56d7ac693408419b41b6d7527d4
babahan <babakulyyevbabahan@gmail.com>
2025-02-18 15:23:32 +0900

    fix script to make not depended on working directory

--- scripts/compress-product-images.sh
+++ scripts/compress-product-images.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-compressImgs="etc/compress-images.mjs"
+CURRENT_DIR=$(dirname "${BASH_SOURCE[0]}")
+compressImgs="$CURRENT_DIR/etc/compress-images.mjs"
 
 if [ -f $compressImgs ]; then
     echo "Compressing.."
