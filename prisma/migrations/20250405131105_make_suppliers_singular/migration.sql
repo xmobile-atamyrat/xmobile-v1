0595385dbd74552004aad49d6a525c02fba2332a
Intizar <intizar.tashov22@gmail.com>
2025-04-05 18:24:19 +0500

    supplier apis

---
+++ prisma/migrations/20250405131105_make_suppliers_singular/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Supplier" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Supplier_pkey" PRIMARY KEY ("id")
+);
