59e6ddb49a51ffcd1563c7d3d7ef6b99aa107132
Intizar <intizar.tashov22@gmail.com>
2025-04-05 22:34:13 +0500

    snackbar in procurement

--- src/pages/procurement/index.page.tsx
+++ src/pages/procurement/index.page.tsx
@@ -1,12 +1,20 @@
 import Layout from '@/pages/components/Layout';
 import { appBarHeight, mobileAppBarHeight } from '@/pages/lib/constants';
+import { SnackbarProps } from '@/pages/lib/types';
 import { useUserContext } from '@/pages/lib/UserContext';
 import Suppliers from '@/pages/procurement/components/Suppliers';
-import { Box, Typography, useMediaQuery, useTheme } from '@mui/material';
+import {
+  Alert,
+  Box,
+  Snackbar,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
 import { GetServerSideProps } from 'next';
 import { useTranslations } from 'next-intl';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   return {
@@ -22,6 +30,9 @@ export default function Procurement() {
   const isMdUp = useMediaQuery(theme.breakpoints.up('md'));
   const t = useTranslations();
   const { user } = useUserContext();
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState<SnackbarProps>();
+
   useEffect(() => {
     if (user?.grade !== 'SUPERUSER') {
       router.push('/');
@@ -42,7 +53,31 @@ export default function Procurement() {
           <Typography fontWeight={600} fontSize={20}>
             {t('procurement')}
           </Typography>
-          <Suppliers />
+
+          <Suppliers
+            setSnackbarMessage={setSnackbarMessage}
+            setSnackbarOpen={setSnackbarOpen}
+          />
+
+          <Snackbar
+            open={snackbarOpen}
+            autoHideDuration={6000}
+            onClose={(_, reason) => {
+              if (reason === 'clickaway') {
+                return;
+              }
+              setSnackbarOpen(false);
+            }}
+          >
+            <Alert
+              onClose={() => setSnackbarOpen(false)}
+              severity={snackbarMessage?.severity}
+              variant="filled"
+              sx={{ width: '100%' }}
+            >
+              {snackbarMessage?.message && t(snackbarMessage.message)}
+            </Alert>
+          </Snackbar>
         </Box>
       )}
     </Layout>
