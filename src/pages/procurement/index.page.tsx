cb123d1808705dd5a2118acc0197aac548380e08
Intizar <intizar.tashov22@gmail.com>
2025-04-05 18:54:57 +0500

    procurement page setup

---
+++ src/pages/procurement/index.page.tsx
@@ -0,0 +1,43 @@
+import Layout from '@/pages/components/Layout';
+import { appBarHeight, mobileAppBarHeight } from '@/pages/lib/constants';
+import { useUserContext } from '@/pages/lib/UserContext';
+import { Box, Typography, useMediaQuery, useTheme } from '@mui/material';
+import { GetServerSideProps } from 'next';
+import { useTranslations } from 'next-intl';
+import { useRouter } from 'next/router';
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  return {
+    props: {
+      messages: (await import(`../../i18n/${context.locale}.json`)).default,
+    },
+  };
+};
+
+export default function Procurement() {
+  const router = useRouter();
+  const theme = useTheme();
+  const isMdUp = useMediaQuery(theme.breakpoints.up('md'));
+  const t = useTranslations();
+  const { user } = useUserContext();
+  console.info(user);
+  return (
+    <Layout handleHeaderBackButton={() => router.push('/')}>
+      {user?.grade === 'SUPERUSER' && (
+        <Box
+          sx={{
+            mt: isMdUp
+              ? `${appBarHeight * 1.25}px`
+              : `${mobileAppBarHeight * 1.25}px`,
+            px: isMdUp ? 4 : 1,
+          }}
+          className="flex flex-col gap-8 w-full h-full"
+        >
+          <Typography fontWeight={600} fontSize={20}>
+            {t('procurement')}
+          </Typography>
+        </Box>
+      )}
+    </Layout>
+  );
+}
