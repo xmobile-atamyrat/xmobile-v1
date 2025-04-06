a375e2403e4b936f97a4bd4aac96f70b5a68a2b5
Intizar <intizar.tashov22@gmail.com>
2025-04-06 22:03:40 +0500

    safe delete /app dir in docker container during build

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,7 +7,7 @@ main() {
         echo "Skipping docker operations. Provide SSH_PASSPHRASE as the first argument to run docker operations"
     else
         docker-compose up -d build_env
-        docker exec build_env sh -c 'rm -rf /app/xmobile-v1 && rm -f /app/xmobile-v1.tar.gz'
+        docker exec build_env sh -c '[ -d /app/xmobile-v1 ] && rm -rf /app/xmobile-v1 && [ -f /app/xmobile-v1.tar.gz ] && rm -f /app/xmobile-v1.tar.gz'
         docker exec -e SSH_PASSPHRASE="$SSH_PASSPHRASE" build_env sh -c '
             eval $(ssh-agent -s) && \
             echo "$SSH_PASSPHRASE" | ssh-add /root/.ssh/id_rsa && \
@@ -16,7 +16,7 @@ main() {
             git clone git@github.com:xmobile-atamyrat/xmobile-v1.git /app/xmobile-v1 && \
             cd /app/xmobile-v1 && \
             rm -rf .env.local && \
-            yarn install && \
+            yarn cache clean && yarn install --force && \
             tar -czvf /app/xmobile-v1.tar.gz /app/xmobile-v1
         '
         docker cp build_env:/app/xmobile-v1.tar.gz .
