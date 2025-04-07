8b750369ec67e81c282871cfa913ef078ff7e6df
Intizar <intizar.tashov22@gmail.com>
2025-04-07 18:03:52 +0500

    empty calculate dialog

---
+++ src/pages/procurement/components/Calculate.tsx
@@ -0,0 +1,16 @@
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from '@mui/material';
+
+export default function Calculate() {
+  return (
+    <Dialog open fullScreen>
+      <DialogTitle>Calculate</DialogTitle>
+      <DialogContent></DialogContent>
+      <DialogActions></DialogActions>
+    </Dialog>
+  );
+}
