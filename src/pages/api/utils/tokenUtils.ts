15f00eedce3af0e01b2d7ed286afdeafa8f8d698
babahan <babakulyyevbabahan@gmail.com>
2025-04-16 01:53:02 +0900

    include grade property in jwt-token structure

--- src/pages/api/utils/tokenUtils.ts
+++ src/pages/api/utils/tokenUtils.ts
@@ -7,11 +7,11 @@ import jwt from 'jsonwebtoken';
 export const ACCESS_SECRET = process.env.ACCESS_TOKEN_SECRET;
 export const REFRESH_SECRET = process.env.REFRESH_TOKEN_SECRET;
 
-export const generateTokens = (userId: string) => {
-  const accessToken = jwt.sign({ userId }, ACCESS_SECRET, {
+export const generateTokens = (userId: string, grade: string) => {
+  const accessToken = jwt.sign({ userId, grade }, ACCESS_SECRET, {
     expiresIn: ACCESS_TOKEN_EXPIRY,
   });
-  const refreshToken = jwt.sign({ userId }, REFRESH_SECRET, {
+  const refreshToken = jwt.sign({ userId, grade }, REFRESH_SECRET, {
     expiresIn: REFRESH_TOKEN_EXPIRY,
   });
 
