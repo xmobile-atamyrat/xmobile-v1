2b30d14a421d7ebd9980d260cee9a28a23ab6fad
babahan <babakulyyevbabahan@gmail.com>
2025-04-27 20:51:23 +0900

    copy nginx config to destination

--- scripts/prod_scripts/restart-nginx.sh
+++ scripts/prod_scripts/restart-nginx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo mv /home/ubuntu/xmobile-v1/nginx/default /etc/nginx/sites-available/default
+sudo cp /home/ubuntu/xmobile-v1/nginx/default /etc/nginx/sites-available/default
 
 if ! sudo nginx -t; then
     echo "NGINX config has errors."
