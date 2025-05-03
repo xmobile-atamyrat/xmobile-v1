3195e83a4080d727c72eb370258d9e9d79342706
Intizar <intizar.tashov22@gmail.com>
2025-05-03 14:34:00 +0500

    refactor search products & suppliers

--- src/pages/procurement/components/EmptyOrder.tsx
+++ src/pages/procurement/components/EmptyOrder.tsx
@@ -1,13 +1,6 @@
 import { SnackbarProps } from '@/pages/lib/types';
-import { useUserContext } from '@/pages/lib/UserContext';
-import {
-  handleProductSearchUtil,
-  handleSupplierSearchUtil,
-} from '@/pages/procurement/lib/utils';
 import {
   Box,
-  Button,
-  debounce,
   Paper,
   Table,
   TableBody,
@@ -20,7 +13,7 @@ import {
 import { ProcurementProduct, Supplier } from '@prisma/client';
 import { JsonValue } from '@prisma/client/runtime/library';
 import { useTranslations } from 'next-intl';
-import { Dispatch, SetStateAction, useCallback, useState } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
 
 interface EmptyOrderProps {
   productQuantities: JsonValue;
@@ -36,76 +29,16 @@ export default function EmptyOrder({
   selectedProducts,
   selectedSuppliers,
   productQuantities,
-  setSnackbarMessage,
-  setSnackbarOpen,
 }: EmptyOrderProps) {
-  const { accessToken } = useUserContext();
   const t = useTranslations();
   const [productQuantity, setProductQuantity] = useState<number[]>(
     productQuantities
       ? (productQuantities as number[]).map((quantity) => quantity)
       : Array(selectedProducts.length),
   );
-  const [searchedProducts, setSearchedProducts] = useState<
-    ProcurementProduct[]
-  >([]);
-  const [searchProductKeyword, setSearchProductKeyword] = useState('');
-  const [searchedSuppliers, setSearchedSuppliers] = useState<Supplier[]>([]);
-  const [searchSupplierKeyword, setSearchSupplierKeyword] = useState('');
-  const [searchedProductsAnchor, setSearchedProductsAnchor] =
-    useState<HTMLElement | null>(null);
-
-  const handleProductSearch = useCallback(
-    debounce(async (keyword: string) => {
-      await handleProductSearchUtil(
-        accessToken,
-        keyword,
-        setSearchedProducts,
-        setSnackbarOpen,
-        setSnackbarMessage,
-      );
-    }, 300),
-    [debounce, accessToken],
-  );
-
-  const handleSupplierSearch = useCallback(
-    debounce(async (keyword: string) => {
-      await handleSupplierSearchUtil(
-        accessToken,
-        keyword,
-        setSearchedSuppliers,
-        setSnackbarOpen,
-        setSnackbarMessage,
-      );
-    }, 300),
-    [debounce, accessToken],
-  );
 
   return (
     <Box className="flex flex-col gap-2">
-      <Box className="flex flex-row justify-between items-center">
-        {/* {SearchBar({
-              handleSearch: handleProductSearch,
-              searchKeyword: searchProductKeyword,
-              setSearchKeyword: setSearchProductKeyword,
-              searchPlaceholder: t('search'),
-              width: '100%',
-            })} */}
-        <Button sx={{ textTransform: 'none' }} variant="outlined">
-          {t('addProducts')}
-        </Button>
-        {/* {SearchBar({
-              handleSearch: handleSupplierSearch,
-              searchKeyword: searchSupplierKeyword,
-              setSearchKeyword: setSearchSupplierKeyword,
-              searchPlaceholder: t('search'),
-              width: '100%',
-            })} */}
-        <Button sx={{ textTransform: 'none' }} variant="outlined">
-          {t('addSuppliers')}
-        </Button>
-      </Box>
-
       <TableContainer component={Paper}>
         <Table>
           <TableHead>
