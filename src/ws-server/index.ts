4757c1998da37127c4b01ebb99b9f7077ab7c5bf
babahan <babakulyyevbabahan@gmail.com>
2025-05-04 22:37:24 +0900

    remove user record, make user-chat relation m2m

--- src/ws-server/index.ts
+++ src/ws-server/index.ts
@@ -3,7 +3,6 @@ import { WebSocketServer, WebSocket } from 'ws';
 import { verifyToken } from '@/pages/api/utils/authMiddleware';
 import { JsonWebTokenError } from 'jsonwebtoken';
 import { ACCESS_SECRET } from '@/pages/api/utils/tokenUtils';
-import { UserRole } from '@prisma/client';
 
 const filepath = 'src/ws-server/index.ts';
 
@@ -12,20 +11,16 @@ const wsServer = new WebSocketServer({ server });
 const port = process.env.WEBSOCKET_SERVER_PORT;
 
 const connections = new Map<string, Set<WebSocket>>();
-const users: Record<UserRole, Set<string>> = {
-  FREE: new Set(),
-  ADMIN: new Set(),
-  SUPERUSER: new Set(),
-};
 
-const handleConnectionClose = (
+const safeCloseConnection = (
+  code: number,
+  reason: string,
   userId: string,
-  grade: UserRole,
   connection: WebSocket,
 ) => {
+  connection.close(code, reason);
   connections.get(userId)?.delete(connection);
-  if (!connections.get(userId).size) connections.delete(userId);
-  users[grade]?.delete(userId);
+  if (!connections.get(userId)?.size) connections.delete(userId);
 };
 
 const verifyConnection = async (
@@ -39,42 +34,56 @@ const verifyConnection = async (
       filepath,
       `. Unauthorized: Missing or invalid header format: ${authHeader}`,
     );
-    connection.close(1008, 'Unauthorized: Missing or invalid token format');
-    return { userId: null, grade: null };
+    safeCloseConnection(
+      1008,
+      'Unauthorized: Missing or invalid format',
+      null,
+      connection,
+    );
+    return { userId: null };
   }
 
   const token = authHeader.split(' ')[1];
-
   try {
-    const { userId, grade } = await verifyToken(token, ACCESS_SECRET);
+    const { userId } = await verifyToken(token, ACCESS_SECRET);
+
     if (!connections.has(userId)) {
       connections.set(userId, new Set());
     }
-    connections.get(userId).add(connection);
-    users[grade as UserRole]?.add(userId);
+    connections.get(userId)?.add(connection);
 
-    return { userId, grade };
+    return { userId };
   } catch (error) {
     console.error(filepath, error);
     if (error instanceof JsonWebTokenError) {
-      console.error(filepath, `Invalid token: ${token};`);
-      connection.close(1008, 'Unauthorized: Missing or invalid token format');
+      console.error(filepath, `InvalidToken: ${token};`);
+      safeCloseConnection(
+        1008,
+        'Unauthorized: Missing or format',
+        null,
+        connection,
+      );
     }
-    return { userId: null, grade: null };
+    return { userId: null };
   }
 };
 
 wsServer.on('connection', async (connection, request) => {
   try {
-    const { userId, grade }: { userId: string; grade: UserRole } =
-      await verifyConnection(connection, request);
-
-    connection.send('Hello from X-mobile 👋');
+    const { userId }: { userId: string } = await verifyConnection(
+      connection,
+      request,
+    );
 
     connection.on('message', () => {});
 
     connection.on('close', () =>
-      handleConnectionClose(userId, grade, connection),
+      safeCloseConnection(
+        1001,
+        'Offline: User Disconnected',
+        userId,
+        connection,
+      ),
     );
   } catch (error) {
     console.error('Connection error:', error);
