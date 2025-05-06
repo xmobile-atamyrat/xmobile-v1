4bc21c635aaf8712bc140dcc190c15a5ed993489
Intizar <intizar.tashov22@gmail.com>
2025-05-06 20:45:09 +0500

    fetch with creds hook to update expired token

--- src/pages/lib/fetch.ts
+++ src/pages/lib/fetch.ts
@@ -1,5 +1,6 @@
 import BASE_URL from '@/lib/ApiEndpoints';
 import { ResponseApi } from '@/pages/lib/types';
+import { useUserContext } from '@/pages/lib/UserContext';
 
 export const fetchWithoutCreds = async <K>(
   path: string,
@@ -37,3 +38,34 @@ export const fetchWithCreds = async <K>(
   ).json();
   return data;
 };
+
+export function useFetchWithCreds() {
+  const { setAccessToken } = useUserContext();
+  return async <K>({
+    accessToken,
+    path,
+    method,
+    body,
+  }: {
+    accessToken: string;
+    path: string;
+    method: string;
+    body?: object;
+  }): Promise<ResponseApi<K>> => {
+    const response = await fetch(`${BASE_URL}${path}`, {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: method === 'GET' ? null : JSON.stringify(body),
+      credentials: 'include',
+    });
+    const newAccessToken = response.headers.get('Authorization');
+    if (newAccessToken) {
+      setAccessToken(newAccessToken.replace('Bearer ', ''));
+    }
+    const data = await response.json();
+    return data;
+  };
+}
