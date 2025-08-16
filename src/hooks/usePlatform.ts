cb9bccbee6c387e6689009bb45fb856e17b0bae3
Eziz Agamyradov <ezizagamyradov@gmail.com>
2025-08-16 18:34:02 +0500

    added usePlatform.ts in hooks folder

--- src/hooks/usePlatform.ts
+++ src/hooks/usePlatform.ts
@@ -1,26 +1,14 @@
 import { useEffect, useState } from 'react';
 
-export type Platform = 'mobile' | 'tablet' | 'web' | 'unknown';
+export type Platform = 'mobile' | 'web';
 
 export function usePlatform(): Platform {
   const [platform, setPlatform] = useState<Platform>('web');
 
   useEffect(() => {
-    if (typeof window === 'undefined') return; // In order to prevent SSR issues
+    if (typeof window === 'undefined') return;
 
-    const ua = navigator.userAgent.toLowerCase();
-
-    if (/ipad|tablet|(android(?!.*mobile))/i.test(ua)) {
-      setPlatform('tablet');
-    } else if (
-      /mobi|iphone|ipod|android.*mobile|blackberry|iemobile|opera mini/i.test(
-        ua,
-      )
-    ) {
-      setPlatform('mobile');
-    } else {
-      setPlatform('web');
-    }
+    setPlatform(window.innerWidth < 900 ? 'mobile' : 'web');
   }, []);
 
   return platform;
