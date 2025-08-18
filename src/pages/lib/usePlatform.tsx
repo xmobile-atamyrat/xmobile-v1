c433e5b007be88a2086fd3e229fce2e629bc695a
Eziz Agamyradov <ezizagamyradov@gmail.com>
2025-08-18 17:02:39 +0500

    Removed hooks folder, added usePlatform next to other hooks, updated _app.page.tsx

---
+++ src/pages/lib/usePlatform.tsx
@@ -0,0 +1,34 @@
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { createContext, ReactNode, useContext } from 'react';
+
+export type Platform = 'mobile' | 'web';
+
+interface PlatformContextType {
+  platform: Platform;
+}
+
+const PlatformContext = createContext<PlatformContextType | undefined>(
+  undefined,
+);
+
+export const PlatformProvider = ({ children }: { children: ReactNode }) => {
+  const theme = useTheme();
+
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+  const platform: Platform = isMobile ? 'mobile' : 'web';
+
+  return (
+    <PlatformContext.Provider value={{ platform }}>
+      {children}
+    </PlatformContext.Provider>
+  );
+};
+
+export const usePlatform = (): Platform => {
+  const context = useContext(PlatformContext);
+  if (!context) {
+    throw new Error('usePlatform must be used within a PlatformProvider');
+  }
+  return context.platform;
+};
