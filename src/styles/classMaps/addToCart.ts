9a0851c984696a4df6f0deb9d920718ed938a29c
Eziz Agamyradov <ezizagamyradov@gmail.com>
2025-08-23 23:13:44 +0500

    Implemented addToCart component

---
+++ src/styles/classMaps/addToCart.ts
@@ -0,0 +1,7 @@
+export const addToCartClasses = {
+  cartIcon: {
+    box: 'rounded-full hover:bg-darkblue shadow',
+    iButton: 'rounded-full bg-blue hover:bg-darkblue shadow',
+  },
+  remButton: 'flex column-reverse items-center',
+};
