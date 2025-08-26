d1254cb0ba428e5e892ddd5b7791cd21b9111f63
Eziz Agamyradov <ezizagamyradov@gmail.com>
2025-08-26 22:58:59 +0500

    finsihed drawer.ts

---
+++ src/styles/classMaps/drawer.ts
@@ -0,0 +1,8 @@
+export const drawerClasses = {
+  box: {
+    web: 'pt-[66px] overflow-auto h-screen',
+    mobile: 'pt-[58px] overflow-auto h-screen',
+  },
+  list: 'flex flex-col',
+  paper: 'h-12 w-full bg-slate-100 flex justify-center',
+};
