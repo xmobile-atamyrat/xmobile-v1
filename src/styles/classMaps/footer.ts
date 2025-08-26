f24fec286ade5afe9b0d54a8bdd1943dcb94deb1
Eziz Agamyradov <ezizagamyradov@gmail.com>
2025-08-26 23:16:51 +0500

    finished footer.ts

---
+++ src/styles/classMaps/footer.ts
@@ -0,0 +1,22 @@
+export const footerClasses = {
+  boxes: {
+    main: {
+      web: 'p-10 bg-gray-100 h-50%',
+      mobile: 'p-10 bg-gray-100 h-[56px]',
+    },
+    main2: 'flex justify-center w-auto h-full',
+    footerStack: {
+      web: 'grid grid-cols-3',
+      mobile: 'grid grid-cols-1',
+    },
+    menu: 'flex flex-col pb-6',
+  },
+  typography: {
+    web: 'text-base font-normal',
+    mobile: 'text-sm font-normal',
+  },
+  typoContact: {
+    web: 'text-xl font-semibold',
+    mobile: 'text-lg font-semibold',
+  },
+};
