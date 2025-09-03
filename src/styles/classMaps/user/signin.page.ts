0a4f84766c8816e593df15ea600c64873331d98b
Eziz Agamyradov <ezizagamyradov@gmail.com>
2025-09-03 09:58:46 +0500

    Updated language json files so signIn page will be understandable in different languages

--- src/styles/classMaps/user/signin.page.ts
+++ src/styles/classMaps/user/signin.page.ts
@@ -1,7 +1,6 @@
 export const signinClasses = {
   boxes: {
     main: 'h-[100vh] flex flex-col mt-[150px] items-center',
-    categories: 'flex flex-col gap-1',
     text: 'flex flex-row justify-center items-center mb-10',
     button: 'flex flex-col gap-2 relative min-h-[70px] mt-4',
     logo: {
@@ -14,17 +13,14 @@ export const signinClasses = {
     mobile: 'flex flex-col w-[90%] h-[400px] rounded-[16px] p-[0px] gap-2',
   },
   paperSignup: {
-    web: 'flex flex-col w-[400px] h-[500px] rounded-[16px] p-2 gap-2',
-    mobile: 'flex flex-col w-[90%] h-[450px] rounded-[16px] p-2 gap-1',
+    web: 'flex flex-col w-[425px] h-[700px] rounded-[16px] p-[30px] gap-2 bg-[#f4f4f4]',
+    mobile: 'flex flex-col w-[90%] h-[450px] rounded-[16px] p-[0px] gap-1',
   },
   buttonSubmit: {
     web: 'normal-case bg-[#ff624c] font-bold h-[60px] rounded-md hover:bg-[#ec4d38] text-base',
     mobile: 'normal-case bg-[#000] font-bold h-[50px] rounded-lg text-base',
   },
-  buttonRedirect: {
-    web: 'normal-case px-2 text-[#000] font-bold',
-    mobile: 'normal-case px-0 text-[#000] font-bold',
-  },
+  buttonRedirect: 'normal-case px-2 text-[#000] font-bold',
   typo: {
     web: 'flex justify-end text-[14px] text-[#666] mb-[3px] mt-[0px]',
     mobile: 'flex justify-center mt-4 text-[#888]',
@@ -34,8 +30,8 @@ export const signinClasses = {
     mobile: 'font-bold text-[30px]',
   },
   label: {
-    web: 'mb-2',
-    mobile: 'ml-[2px]',
+    web: 'mb-1 mt-1',
+    mobile: 'ml-[2px] mt-[10px]',
   },
   textField: {
     web: 'bg-white rounded-lg h-[60px] [&_.MuiOutlinedInput-notchedOutline]:border-[#fff] hover:[&_.MuiOutlinedInput-notchedOutline]:border-[#ff624c] focus-within:[&_.MuiOutlinedInput-notchedOutline]:border-[#ff624c]',
