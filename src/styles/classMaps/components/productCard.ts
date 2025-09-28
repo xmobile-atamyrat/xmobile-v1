f107c604a5851e21c0ddc6580b25054e371e9129
Eziz Agamyradov <ezizagamyradov@gmail.com>
2025-09-28 22:51:48 +0500

    Updated product cards

--- src/styles/classMaps/components/productCard.ts
+++ src/styles/classMaps/components/productCard.ts
@@ -1,19 +1,19 @@
 export const productCardClasses = {
   card: {
-    web: 'w-[200px] h-[300px] hover:shadow-xl',
-    mobile: 'w-[47%] h-[260px] hover:shadow-xl',
+    web: 'w-[280px] h-auto border-[1px] rounded-[10px] px-2 py-2 relative border-[#f0f0f0] hover:border-[#30303040]',
+    mobile: 'w-[182px] h-[300px] gap-[8px] rounded-[12px] p-0 border-0',
   },
   boxes: {
     main: 'relative h-full w-full flex flex-col justify-between p-1',
-    addCard: 'w-full overflow-y-scroll overflow-x-hidden',
+    price: 'flex items-end justify-between',
   },
   typo: {
-    web: 'text-lg mt-1',
-    mobile: 'text-sm mt-1',
+    web: 'text-[20px] mt-[10px] font-[600] leading-[30px]',
+    mobile: 'text-[12px] font-[500] mt-[8px]',
   },
   typo2: {
-    web: 'font-semibold text-base',
-    mobile: 'font-semibold text-sm',
+    web: 'font-[600] text-[24px] leading-[30px]',
+    mobile: 'font-[600] text-[14px] leading-[100%] tracking-0',
   },
   typo3: {
     web: 'flex justify-center text-center text-xl font-medium',
@@ -24,5 +24,9 @@ export const productCardClasses = {
     mobile: 'w-[24px] h-[24px]',
   },
   cardActions: 'w-full flex justify-center items-end',
-  cardMedia: 'h-[100px] w-auto p-0',
+  cardMedia: {
+    web: 'h-[315px] w-auto p-0 mt-[24px] rounded-[5px]',
+    mobile:
+      'w-full h-[241px] border-[2px] border-[#f2f2f2] rounded-[8px] object-cover',
+  },
 };
