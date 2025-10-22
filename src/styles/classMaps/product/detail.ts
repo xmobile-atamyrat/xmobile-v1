0f764020793f01e3c4995d56207c1ce6c63bf233
Eziz Agamyradov <ezizagamyradov@gmail.com>
2025-10-23 00:38:47 +0500

    moved some specs next to the image

--- src/styles/classMaps/product/detail.ts
+++ src/styles/classMaps/product/detail.ts
@@ -20,14 +20,15 @@ export const detailPageClasses = {
       mobile:
         'flex flex-row w-full min-h-[11.7vw] p-0 justify-between items-center my-[20px]',
     },
-    video: {
-      web: 'flex p-2 w-1/2',
-      mobile: 'flex p-2',
-    },
+    video: 'flex flex-row w-[10vw] h-auto ml-[1vw]',
     detail: {
       web: 'w-[79vw] h-auto flex flex-col justify-center items-center mb-[90px] m-auto p-0 gap-0',
       mobile:
         'min-w-[88.78vw] h-auto flex p-0 gap-0 justify-center items-start mx-auto mt-[-30px] mb-[30px]',
+    },
+    detailSide: {
+      web: 'w-[35vw] h-auto flex flex-col',
+      mobile: 'hidden',
     },
   },
   circProgress: {
@@ -49,13 +50,13 @@ export const detailPageClasses = {
         'text-[clamp(2vw,_3vw,_13px)] font-regular leading-[20px] tracking-normal opacity-[40%]',
     },
     desc: {
-      web: 'text-[1.05vw] leading-[30px] font-semibold text-[#303030]',
+      web: 'text-[1.05vw] leading-[30px] font-semibold text-[#303030] ml-[0.05vw]',
       mobile:
         'font-bold text-[clamp(2vw,_3vw,_13px)] leading-[20px] tracking-normal opacity-[40%]',
     },
   },
   link: {
-    web: 'px-3 pb-3 flex items-center flex-row',
+    web: 'flex flex-row',
     mobile: 'px-3 pb-3 flex items-center flex-col',
   },
   productName: {
@@ -68,7 +69,7 @@ export const detailPageClasses = {
     mobile: 'h-[70vw] object-contain',
   },
   list: {
-    web: 'p-0 pb-10',
+    web: 'p-0 mb-[3vw]',
     mobile: 'hidden',
   },
   specs: {
@@ -89,7 +90,7 @@ export const detailPageClasses = {
       mobile: 'flex flex-row w-full h-auto gap-[2.3vw]',
     },
     head: {
-      web: 'w-[13vw]',
+      web: 'w-[12vw]',
       mobile: 'w-[35vw] h-auto',
     },
     val: {
@@ -97,8 +98,16 @@ export const detailPageClasses = {
       mobile: 'max-w-[50vw]',
     },
     name: {
-      web: '',
+      web: 'flex flex-row w-[50vw]',
       mobile: 'w-[50vw] h-auto',
     },
+  },
+  detailSide: {
+    part: 'w-[35vw] flex flex-row my-[0.4vw]',
+    head: 'w-[10vw]',
+    val: 'flex flex-col w-[25vw]',
+    desc: 'text-[1.05vw] leading-[30px] font-semibold text-[#303030]',
+    font2:
+      'text-[1.05vw] leading-[30px] font-regular tarcking-normal text-[#303030]',
   },
 };
