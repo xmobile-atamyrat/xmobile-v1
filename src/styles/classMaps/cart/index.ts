6fab037facf340379fc496659b29fa9729546cf8
Eziz Agamyradov <ezizagamyradov@gmail.com>
2025-11-05 23:26:04 +0500

    empty cart mobile changes

--- src/styles/classMaps/cart/index.ts
+++ src/styles/classMaps/cart/index.ts
@@ -5,12 +5,30 @@ export const cartIndexClasses = {
   },
   prodCart: 'flex flex-wrap gap-4 w-full p-3 justify-center',
   link: 'flex flex-row justify-center items-center gap-1 py-2 no-underline mx-2',
-  iconButton:
-    'text-[#fff] font-bold text-[20px] leading-[30px] tracking-normal m-0',
+  iconButton: {
+    web: 'text-[#fff] font-bold text-[20px] leading-[30px] tracking-normal m-0',
+    mobile: 'text-[#fff] font-medium text-[16px] leading-full tracking-normal',
+  },
   breadcrumbs: {
     web: 'ml-10 mt-4',
     mobile: 'hidden',
   },
   breadcrumbsText:
     'no-underline text-[#303030] text-[16px] leading-[24px] tracking-normal',
+  emptyCart: {
+    img: {
+      web: 'w-[22.34vw] h-[18.64vw] mx-auto my-[3.9vw]',
+      mobile: 'w-[180px] h-[150px] mx-auto mt-[100px]',
+    },
+    typo: {
+      web: 'font-medium text-[30px] leading-[24px] tracking-normal text-[#000] text-center mb-[3.125vw]',
+      mobile:
+        'mt-[38px] font-medium text-[20px] leading-[28px] tracking-normal text-[#000] text-center mb-[35px]',
+    },
+    link: {
+      web: 'min-w-[11.875vw] h-[3.22vw] bg-[#ff624c] rounded-[10px] py-[16px] px-[40px] gap-[10px] flex justify-center items-center no-underline',
+      mobile:
+        'w-[88.7vw] h-[11.2vw] max-h-[48px] bg-[#ff624c] rounded-[12px] py-[4px] px-[20px] gap-[16px] flex justify-center items-center no-underline',
+    },
+  },
 };
