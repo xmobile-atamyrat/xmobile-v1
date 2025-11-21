b7fdeab7d1baf15f8a284644d85317da55c32227
Eziz Agamyradov <ezizagamyradov@gmail.com>
2025-11-21 22:46:13 +0500

    design implementation

--- src/styles/classMaps/components/footer.ts
+++ src/styles/classMaps/components/footer.ts
@@ -1,6 +1,6 @@
 export const footerClasses = {
   boxes: {
-    main: {
+    mainWeb: {
       web: 'flex flex-col w-[78.95vw] h-[20%] mt-[80px] mx-auto',
       mobile: 'hidden',
     },
@@ -23,4 +23,13 @@ export const footerClasses = {
   },
   socialLinks:
     'font-regular text-[16px] leading-[24px] tracking-normal text-[#303030]',
+  mainMobile: {
+    web: 'hidden',
+    mobile:
+      'justify-between bg-[#fff] w-full h-[60px] px-[40px] py-[20px] shadow-[0_0_70px_0_#0000001A] flex items-center',
+  },
+  mainBox: {
+    web: 'flex flex-col w-full',
+    mobile: 'flex flex-col w-full mt-[100px]',
+  },
 };
