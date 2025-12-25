067a14ecc9670f8ca3ab3448fd861ad2bd461315
Intizar <intizar.tashov22@gmail.com>
2025-12-25 01:03:38 +0000

    count orders model

---
+++ prisma/migrations/20251225005507_count_orders_model/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "order_number_counter" (
+    "id" INTEGER NOT NULL DEFAULT 1,
+    "counter" INTEGER NOT NULL DEFAULT 1,
+
+    CONSTRAINT "order_number_counter_pkey" PRIMARY KEY ("id")
+);
