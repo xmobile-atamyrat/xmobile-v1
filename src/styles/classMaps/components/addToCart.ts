8f96b92eebaf7d6176c270db0f0d7f53a79c991c
Eziz Agamyradov <ezizagamyradov@gmail.com>
2026-01-03 19:44:01 +0500

    id-page centering fix

--- src/styles/classMaps/components/addToCart.ts
+++ src/styles/classMaps/components/addToCart.ts
@@ -24,15 +24,16 @@ export const addToCartClasses = {
   },
   detail: {
     box: {
-      web: 'flex flex-row min-w-[30.78vw] h-[3.5vw] mt-[7.5vw]',
+      web: 'flex flex-row w-[28vw] h-[3.5vw] mt-[7.5vw] justify-between',
       mobile: 'flex w-[88.7vw] mx-auto mb-[30px]',
     },
     addToCart: {
-      web: 'max-w-[300px] h-[3.5vw] bg-[#ff624c] gap-[10px] rounded-[10px] py-[16px] px-[40px] ml-[5.41vw] items-center hover:bg-[#ec4d38]',
+      web: 'max-w-[15vw] h-[3.5vw] bg-[#ff624c] gap-[10px] rounded-[10px] py-[16px] px-[2vw] items-center hover:bg-[#ec4d38]',
       mobile:
         'w-[88.7vw] bg-[#1C1B1B] text-[#fff] h-[clamp(20px,_11.2vw,_52px)] rounded-[15px] px-[10px] gap-[8px] mx-auto',
     },
-    quantityButton: 'w-[24px] h-[24px] rounded-[100%] bg-[#f4f4f4] text-[#000]',
+    quantityButton:
+      'w-[1.5vw] h-[1.5vw] rounded-[100%] bg-[#f4f4f4] text-[#000]',
     addToCartText: {
       web: 'justify-center font-[700] text-[1vw] leading-[30px] tracking-0 text-[#fff]',
       mobile:
@@ -40,7 +41,7 @@ export const addToCartClasses = {
     },
   },
   iconButton: {
-    web: 'w-[2.9vw] h-[2.9vw] bg-[#f4f4f4]',
+    web: 'w-[2.5vw] h-[2.5vw] bg-[#f4f4f4]',
     mobile: 'hidden',
   },
   inputDet: {
