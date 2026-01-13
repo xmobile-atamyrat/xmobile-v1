e4b3aa0d364c463216b4c33a2e55574e87d5485d
Intizar <intizar.tashov22@gmail.com>
2026-01-13 01:50:54 +0000

    improve backup prod data script

--- scripts/backup-production-data.sh
+++ scripts/backup-production-data.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-scp xmobile:/home/ubuntu/db_backup/db_backup.sql ./backup/db_backup.sql
-scp xmobile:/home/ubuntu/tar-file/images.tar.gz ./backup/images.tar.gz
+arg=$1
+
+copy_db() {
+	scp xmobile:/home/ubuntu/db_backup/db_backup.sql ./backup/db_backup.sql
+}
+
+copy_images() {
+	scp xmobile:/home/ubuntu/tar-file/images.tar.gz ./backup/images.tar.gz
+}
+
+if [ "$arg" == "db" ]; then
+	copy_db
+elif [ "$arg" == "images" ]; then
+	copy_images
+elif [ "$arg" == "all" ]; then
+	copy_db
+	copy_images
+else
+	echo "Provide an arg: db, images, all"
+fi
