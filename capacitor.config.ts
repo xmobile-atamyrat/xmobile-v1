503bc90b1a525389fe387f4c3c69228048a86a94
Intizar <intizar.tashov22@gmail.com>
2026-01-16 03:02:36 +0000

    init capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'tm.com.xmobile.app',
+  appName: 'Xmobile',
+  webDir: '.next',
+};
+
+export default config;
