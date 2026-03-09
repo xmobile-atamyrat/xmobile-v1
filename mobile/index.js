6b2615a796ca07b6c2e98db3ba703aaf92734c8f
Eziz Agamyradov <ezizagamyradov@gmail.com>
2026-03-09 16:12:24 +0500

    bg message handling

--- mobile/index.js
+++ mobile/index.js
@@ -2,8 +2,14 @@
  * @format
  */
 
+import messaging from '@react-native-firebase/messaging';
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
+
+// Register background handler
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handled in the background!', remoteMessage);
+});
 
 AppRegistry.registerComponent(appName, () => App);
