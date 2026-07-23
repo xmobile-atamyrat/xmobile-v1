a284855ec1ee5bf23de23559cc28b05ac6a569f0
Eziz Agamyradov <ezizagamyradov@gmail.com>
2026-07-23 15:00:46 +0500

    hotfix: fix narrowed desc edit

--- src/styles/classMaps/components/addEditProductDialog.ts
+++ src/styles/classMaps/components/addEditProductDialog.ts
@@ -13,6 +13,10 @@ export const addEditProductDialogClasses = {
         web: 'flex flex-col w-[600px] gap-2 p-2',
         mobile: 'flex flex-col w-[300px] gap-2 p-2',
       },
+      descGrid: {
+        web: 'grid grid-cols-2 gap-3 w-full max-w-[1000px] p-2',
+        mobile: 'flex flex-col w-[300px] gap-2 p-2',
+      },
     },
     fullRel: 'h-full w-full p-2 relative',
     absZero: 'absolute right-0 top-0',
@@ -20,6 +24,10 @@ export const addEditProductDialogClasses = {
   textField: {
     usual: {
       web: 'my-1 mr-2 w-1/3 min-w-[250px]',
+      mobile: 'my-1 w-full',
+    },
+    description: {
+      web: 'my-1 w-full',
       mobile: 'my-1 w-full',
     },
     price: {
