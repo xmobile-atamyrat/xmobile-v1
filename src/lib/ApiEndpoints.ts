3a555948668647828402b8c2304f9399c08484d2
Intizar <intizar.tashov22@gmail.com>
2026-08-23 23:52:25 +0100

    add staging option to base url

--- src/lib/ApiEndpoints.ts
+++ src/lib/ApiEndpoints.ts
@@ -1,6 +1,12 @@
-const BASE_URL =
-  process.env.APP_ENV === 'production'
-    ? 'https://xmobile.com.tm'
-    : `http://${process.env.NEXT_PUBLIC_HOST ?? 'localhost'}:${process.env.NEXT_PUBLIC_PORT ?? 3000}`;
+// eslint-disable-next-line import/no-mutable-exports
+let BASE_URL = '';
+
+if (process.env.APP_ENV === 'production') {
+  BASE_URL = 'https://xmobile.com.tm';
+} else if (process.env.APP_ENV === 'staging') {
+  BASE_URL = 'https://216.250.13.115:3001';
+} else {
+  BASE_URL = `http://${process.env.NEXT_PUBLIC_HOST ?? 'localhost'}:${process.env.NEXT_PUBLIC_PORT ?? 3000}`;
+}
 
 export default BASE_URL;
